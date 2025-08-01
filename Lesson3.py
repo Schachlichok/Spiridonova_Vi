07798104baeb145c3c3f68a21c69586aa72381ae
Spiridonova_Vi <kizischik@gmail.com>
2025-08-01 17:21:24 +0300

    Закрепление темы Строки

--- Lesson3.py
+++ Lesson3.py
@@ -171,3 +171,120 @@ print (text.replace("Python","программирование"))
 s = "банан, яблоко, банан, груша, банан"
 print("банан" in s)  # Проверка наличия (True)
 print(s.count("банан"))
+
+# Повторение
+# Задача 1 (Срезы строк)
+# Дана строка:
+# text = "автоматизация"
+# Выведи подстроку с 3-го по 7-й символ включительно.
+
+text = "автоматизация"
+print(text[2:7])
+
+# Задача 2 (Методы строк)
+# Дана строка:
+# s = "   Hello, World!   "
+# Удали лишние пробелы в начале и конце строки и выведи результат.
+
+s = "   Hello, World!   "
+print(s.strip())
+
+# Задача 3 (Конкатенация)
+# Даны две строки:
+# part1 = "Python"
+# part2 = " - это сила!"
+# Объедини их в одну строку без пробелов между переменными в коде.
+
+part1 = "Python"
+part2 = "- это сила!"
+print(part1+part2)
+
+# Задача 4 (F-строки)
+# Даны переменные:
+# city = "Москва"
+# population = 12_000_000
+# Выведи строку: "В городе Москва живёт 12_000_000 человек".
+
+city = "Москва"
+population = 12_000_000
+print(f"В городе {city} живёт {population} человек")
+
+# Задача 5 (Поиск подстроки)
+# Дана строка:
+# text = "Я изучаю Python и Python это круто"
+# Найди и выведи индекс первого вхождения слова "Python".
+
+text = "Я изучаю Python и Python это круто"
+print(text.find("Python"))
+
+# Задача 6 (Замена подстроки)
+# Дана строка:
+# sentence = "Я люблю Java"
+# Замени слово "Java" на "Python" и выведи результат.
+
+sentence = "Я люблю Java"
+print (sentence.replace("Java","Python"))
+
+# Задача 7 (Регистр символов)
+# Дана строка:
+# word = "ПРОГРАММИРОВАНИЕ"
+# Приведи все символы к нижнему регистру и выведи результат.
+
+word = "ПРОГРАММИРОВАНИЕ"
+print(word.lower())
+
+# Задача 8 (Разделение строки)
+# Дана строка:
+# data = "яблоко,банан,апельсин"
+# Разбей строку по запятым в список фруктов и выведи список.
+
+data = "яблоко,банан,апельсин"
+print(data.split(","))
+
+# Задача 9 (Длина строки)
+# Дана строка:
+# password = "Secret123"
+# Выведи длину этой строки.
+
+password = "Secret123"
+print(len(password))
+
+# Задача 10 (Повторение строки)
+# Дана строка:
+# symbol = "*"
+# Выведи эту строку повторённую 10 раз подряд.
+
+symbol = "*"
+print(symbol * 10)
+
+password = "erpr"
+print(password == password[::-1])
+
+password = "0123456789"
+print(password[::3])
+
+password = "0123456789!"
+print(password.strip("!"))
+print(id(password))
+
+filename = "document.pdf"
+print(filename.startswith("doc"))
+print(filename.endswith(".pdf"))
+
+text = "https://example.com"
+print ("Веб-адрес" if text.startswith("http://") or text.startswith("https://") else "Неизвестный формат")
+
+data = "  REPORT_2023.txt  "
+print(data.strip().startswith("REPORT"))
+print(data.strip().endswith(".txt"))
+
+def divide(a, b):
+    assert b!=0, "b равно нулю"
+    return a/b
+print(divide(int(input ("Введите а")),int(input ("Введите b"))))
+
+def check_login(login):
+    assert len(login)>=5, "Логин должен быть минимум 5 символов"
+    assert " " not in login, "Логин должен быть без пробелов"
+    return True
+print(check_login("muxomor122"))
